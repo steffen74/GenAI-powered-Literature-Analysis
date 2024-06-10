7a60cf3ff482456a2e873d78b3de8c1c358fef42
Kaan Apaydin <151673735+kaan1derful@users.noreply.github.com>
2024-06-10 20:36:08 +0200

    Add files via upload

---
+++ src/openFile.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+def open_file(file_path):
+    """
+    Opens a file using the default program associated with the file type on Windows.
+
+    Parameters:
+    file_path (str): The path to the file to be opened.
+    """
+    try:
+        if os.path.exists(file_path):
+            # Using start command to open the file with the default application
+            subprocess.run(['start', '', file_path], shell=True, check=True)
+            print(f"File {file_path} opened successfully.")
+        else:
+            print(f"File {file_path} does not exist.")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to open file {file_path}. Error: {e}")
+
+
+# Example usage:
+file_path = r'C:\Users\Kaan Apaydin\Downloads\main.pdf'
+open_file(file_path)
